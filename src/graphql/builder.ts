00f6230fa055b091c34d3b541f5ac4b99ad76660
Brandon Eastwell <92370206+BrandonEastwell@users.noreply.github.com>
2026-07-23 22:23:58 +0100

    added setup for pothos schema builder

--- src/graphql/builder.ts
+++ src/graphql/builder.ts
@@ -1,16 +1,18 @@
 import SchemaBuilder from "@pothos/core";
 import PrismaPlugin from "@pothos/plugin-prisma";
 
-import type PrismaTypes from "../generated/pothos-types.js";
-import type { GraphQLContext } from "../context.js";
-import { prisma } from "../prisma.js";
+import PrismaTypes, {getDatamodel} from "@pothos/plugin-prisma/generated";
+import {prisma} from "../db/prisma";
 
-export const builder = new SchemaBuilder<{
-    Context: GraphQLContext;
-    PrismaTypes: PrismaTypes;
+const builder = new SchemaBuilder<{
+    PrismaTypes: PrismaTypes; // This gives the builder all the type information about your prisma schema
 }>({
     plugins: [PrismaPlugin],
     prisma: {
         client: prisma,
+        dmmf: getDatamodel(),
+        filterConnectionTotalCount: true,
+        // warn when not using a query parameter correctly
+        onUnusedQuery: process.env.NODE_ENV === 'production' ? null : 'warn',
     },
 });
