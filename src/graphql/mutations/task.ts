6be3fcd0d530e24be0f5512b9a2e7c028aeee24f
Brandon Eastwell <92370206+BrandonEastwell@users.noreply.github.com>
2026-07-24 05:56:00 +0100

    added update task mutation field

--- src/graphql/mutations/task.ts
+++ src/graphql/mutations/task.ts
@@ -1,4 +1,5 @@
 import {builder} from "../builder";
+import type {Prisma} from "@prisma/client";
 import {z} from "zod";
 
 builder.mutationField("addTask", (t) => t.prismaField({
@@ -46,4 +47,48 @@ builder.mutationField("deleteTask", (t) => t.prismaField({
                 id: args.id
             }
         })
+}))
+
+builder.mutationField("updateTask", (t) => t.prismaField({
+    type: "Task",
+    args: {
+        id: t.arg.id({
+            required: true,
+            validate: {
+                schema: z.string().uuid("id must be a valid UUID"),
+            },
+        }),
+        title: t.arg.string({
+            required: false,
+            validate: {
+                type: "string",
+                minLength: 1,
+                maxLength: 100
+            }
+        }),
+        completed: t.arg.boolean({
+            required: false
+        })
+    },
+    validate: [(args) => args.title != null || args.completed != null, {
+        message: "At least one of title or completed must be provided"
+    }],
+    resolve: (query, parent, args, ctx, info) => {
+        const data: Prisma.TaskUpdateInput = {};
+
+        if (args.title != null) {
+            data.title = args.title;
+        }
+
+        if (args.completed != null) {
+            data.completed = args.completed;
+        }
+
+        return ctx.prisma.task.update({
+            ...query,
+            where: { id: args.id },
+            data,
+        })
+    }
+
 }))
